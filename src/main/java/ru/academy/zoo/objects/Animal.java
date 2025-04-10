fddad93c05f6fcbda65a52c968d47a648823d633
RIArobot <qp10@list.ru>
2025-04-10 11:55:31 +0400

    Update Animal.java

--- src/main/java/ru/academy/zoo/objects/Animal.java
+++ src/main/java/ru/academy/zoo/objects/Animal.java
@@ -12,7 +12,7 @@ public abstract class Animal {
 
     protected final String name;
 
-    //TODO перевести на Date - дата рождения 555
+    //TODO перевести на Date - дата рождения 123
     protected int age;
 
     public Animal(String name_, int age_) throws AgeException {
